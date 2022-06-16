b2f9e71c046894eb4469aec6efe4f89d913aa7ad
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-16 16:49:42 +0300

    Добавьте файлы проекта.

---
+++ CS9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "exit";
+            string tempString;
+
+            while (true)
+            {
+                Console.WriteLine("Для продолжения нажмите 'Enter'. Для выхода введите " + exit);
+                tempString = Console.ReadLine();
+
+                if (tempString == exit)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
